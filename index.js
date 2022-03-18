51e2c5c0ded9c1b2e9e99705f037012558b43806
FilipposGravanis <grampouni20@gmail.com>
2022-03-18 22:20:29 +0200

    per letter coloring

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const wordList = ['word1', 'asddasd']
+const wordList = ['word1', 'asddasd',"why","yes","no"]
 let timeLeft = 60
+let wordIndex=0
 const input = document.getElementById("input")
 input.style.height = "50px"
 input.style.width = "100%"
@@ -7,6 +8,7 @@ input.style.fontSize = '20px'
 input.style.border = "1px solid blue"
 input.style.borderRadius = '10px'
 let isCountdownStarted = false
+let isGameOver=false
 function timeLeftCountDown() {
     if (isCountdownStarted == false) {
         isCountdownStarted = true
@@ -37,6 +39,7 @@ displayWordsContainer.style.gap = "10px"
 
 let displayedWordsList = []
 let wordListLength = wordList.length
+
 for (i = 0; i < wordList.length; i++) {
     let wordplaceholder = new word(wordList[i], false)
     displayedWordsList.push(wordplaceholder)
@@ -74,6 +77,7 @@ input.addEventListener('keydown', function (keyPress) {
     setTimeout(() => {
         if (keyPress.key === ' ' && timeLeft > 0 && input.value.length > 1 && input.value[1] != '' && typedWordCount < wordList.length) {
             typedWord = input.value
+            wordIndex=0
             if (typedWordCount != 0) {
                 displayedWordsList[0].text.style.backgroundColor = "white"
             }
@@ -116,9 +120,29 @@ input.addEventListener('keydown', function (keyPress) {
 
             }
             
+            
 
             input.value = ""
             input.focus()
+        }
+        if((timeLeft === 0 || typedWordCount>=wordListLength) && !isGameOver ){
+            isGameOver=true
+            let wpm=(60)*correctWordsCount
+            const wpmElement=document.createElement('p')
+            document.body.append(wpmElement)
+            wpmElement.innerText=wpm
+            
+        }
+        if(keyPress.key!==' ' && timeLeft > 0  && typedWordCount < wordList.length){
+                if(input.value!=displayedWordsList[typedWordCount].text.innerText.substr(0,input.value.length)){
+                    displayedWordsList[typedWordCount].text.style.color="red"
+                
+                }
+                else{
+                    displayedWordsList[typedWordCount].text.style.color="black"
+                }
+                console.log(input.value,displayedWordsList[typedWordCount].text.innerText.substr(0,input.value.length))
+                wordIndex=wordIndex + 1
         }
     }, 1)
 
